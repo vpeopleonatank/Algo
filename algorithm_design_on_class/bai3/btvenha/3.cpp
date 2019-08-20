dbd120c8e2334be80bb0f9667b4c32089123ef30
Trần Quang Khải <vpeopleonatank@gmail.com>
2019-08-20 22:11:33 +0700

    commit

--- algorithm_design_on_class/bai3/btvenha/3.cpp
+++ algorithm_design_on_class/bai3/btvenha/3.cpp
@@ -2,21 +2,25 @@
 
 using namespace std;
 
-string Scoretostring1(char str[100]) {
-    int i = 0;
+string Scoretostring(char str[100]) {
+    int i = 0, lnum = 0;
     int a[100];
     while (str[i] != '.') {
-        a[i] = str[i] - 48;
+      lnum++;
+      a[i] = str[i] - 48;
     }
 
 }
 
 int main() {
-    freopen("3.in", "r", stdin);
-    freopen("3.out", "w", stdout);
+  string a[10] = {"khong", "mot", "hai", "ba", "bon", "nam", "sau", "bay", "tam", "chin"};
+    //freopen("3.in", "r", stdin);
+    //freopen("3.out", "w", stdout);
     float num;
     cin >> num;
     char str[100];
-    sprintf(str, "%f", num);
-    cout << Scoretostring1(num);
+    //sprintf(str, "%f", num);
+    //cout << Scoretostring(str);
+    cout << a[0];
 }
+
