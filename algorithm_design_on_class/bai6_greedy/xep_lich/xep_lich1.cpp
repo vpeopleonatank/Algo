b270c6210ebb7959173eea2a962acd6e25080e8f
Trần Quang Khải <vpeopleonatank@gmail.com>
2019-09-30 12:43:25 +0700

    complete color graphing and xep lich

---
+++ algorithm_design_on_class/bai6_greedy/xep_lich/xep_lich1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct Activity {
+  int start, finish;
+};
+
+bool CompareActivity(Activity a1, Activity a2) {
+  return a1.start < a2.start;
+}
+
+void SelectActivity(Activity act[], int num_of_activities) {
+  sort(act, act+num_of_activities, CompareActivity);
+
+  int i = 0;
+  cout << i << " ";
+
+  for (int j = 1; j < num_of_activities; j++) {
+    if (act[j].start > act[i].finish) {
+      cout << j << " ";
+      i = j;
+    }
+  }
+  cout << endl;
+}
+
+int main() {
+  freopen("in.xeplich", "r", stdin);
+  freopen("out.xeplich1", "w", stdout);  
+
+  int test_case;
+  cin >> test_case;
+  while(test_case--) {
+    int num_of_activities;
+    cin >> num_of_activities;
+    Activity act[num_of_activities];
+    SelectActivity(act, num_of_activities);
+  }
+}
+
